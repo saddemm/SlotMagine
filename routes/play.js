85639c61318477fa59bfc5e97a4052698ff84e54
Saddem Anane <saddem.anene@esprit.tn>
2018-01-21 19:34:24 +0100

    TEst for ip

--- routes/play.js
+++ routes/play.js
@@ -5,16 +5,23 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/:rand', function(req, res, next) {
 
-  console.log('XXXX REFEEERERR XXXXX');
+  var rand = req.params.rand;
   var ip = req.headers['x-forwarded-for'] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       req.connection.socket.remoteAddress;
 
-  console.log(ip);
-  console.log('XXXX REFEEERERR XXXXX');
 
-  res.render('play', {rand : req.params.rand});
+  var cutIp = ip.substring(0, 13);
+
+  if (cutIp == '::ffff:66.249'){
+    console.log('XXXX  Its google XXXXXXX');
+  }else{
+    console.log('XXXXXXXXX c bon XXXXXXXXXx');
+  }
+
+
+  res.render('play', {rand : rand});
 });
 
 module.exports = router;
